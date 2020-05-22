b25e0fc42a80a872e90191a01d217f06c99e594c
JavierCollipal <jcollipal1212@gmail.com>
2020-05-22 13:48:13 -0400

    build and run works with spring initializer

---
+++ src/main/java/com/example/gatos_spring/GatosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gatos_spring/GatosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
